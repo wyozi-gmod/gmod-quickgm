27a3c1d011024ab35a6a05cb7b44e1bed740571c
JarosLucky <snyashka@gmail.com>
2019-12-15 17:36:59 +0700

    pairs -> ipairs

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -11,24 +11,24 @@ local function LoadModuleFolder(modulenm)
 	local files, folders = file.Find(full_folder .. "*", "LUA")
 
 	-- Recursive file search
-	for _, ifolder in pairs(folders) do
+	for _, ifolder in ipairs(folders) do
 		LoadModuleFolder(modulenm .. ifolder .. "/")
 	end
 
-	for _, shfile in pairs(file.Find(full_folder .. "sh_*.lua", "LUA")) do
+	for _, shfile in ipairs(file.Find(full_folder .. "sh_*.lua", "LUA")) do
 		GM.Debug("Loading sh module " .. full_folder ..shfile)
 		if SERVER then AddCSLuaFile(full_folder .. shfile) end
 		include(full_folder .. shfile)
 	end
 
 	if SERVER then
-		for _, svfile in pairs(file.Find(full_folder .. "sv_*.lua", "LUA")) do
+		for _, svfile in ipairs(file.Find(full_folder .. "sv_*.lua", "LUA")) do
 			GM.Debug("Loading sv module " .. svfile)
 			include(full_folder .. svfile)
 		end
 	end
 
-	for _, clfile in pairs(file.Find(full_folder .. "cl_*.lua", "LUA")) do
+	for _, clfile in ipairs(file.Find(full_folder .. "cl_*.lua", "LUA")) do
 		GM.Debug("Loading cl module " .. clfile)
 		if SERVER then AddCSLuaFile(full_folder .. clfile) end
 		if CLIENT then include(full_folder .. clfile) end
